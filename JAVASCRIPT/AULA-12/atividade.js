dfb401468dccce8340ba9338ed76189ffde49ea2
juulianuness <juuliacristiny@gmail.com>
2025-07-08 18:08:21 -0300

    Aula-12

---
+++ JAVASCRIPT/AULA-12/atividade.js
@@ -0,0 +1,38 @@
+let nome = "Julia";
+let idade = 29;
+let altura = 1.58;
+
+console.log("Exercício 01: Meu nome é", nome,", tenho", idade, "anos", "e minha altura é", altura,"m.");
+
+
+let numero1 = 9;
+let numero2 = 1;
+
+let soma = numero1 + numero2 
+
+console.log("Exercício 02: A soma é:", soma)
+
+
+let nota1 = 8.8;
+let nota2 = 4.8;
+let nota3 = 3.5;
+
+let media = (nota1 + nota2 + nota3) / 3;
+console.log("Exercício 03: A média é:", media)
+
+
+
+let valor1 = 10;
+let valor2 = 5;
+let valor3 = 2;
+let resultado = (valor1 + valor2) * valor3 / 2;
+
+console.log("Exercício 04: O resultado é:", resultado)
+
+
+let novaIdade = 25;
+
+
+console.log( "Exercício 05: É maior de idade: ", novaIdade >= 18);
+
+
