d045fb5a5773c2413928b3839169abbcdc50721b
juulianuness <juuliacristiny@gmail.com>
2025-07-09 09:48:26 -0300

    js/atividade

--- JAVASCRIPT/AULA-12/atividade.js
+++ JAVASCRIPT/AULA-12/atividade.js
@@ -4,6 +4,7 @@ let altura = 1.58;
 
 console.log("Exercício 01: Meu nome é", nome,", tenho", idade, "anos", "e minha altura é", altura,"m.");
 
+// --------------------------------------------------------
 
 let numero1 = 9;
 let numero2 = 1;
@@ -12,6 +13,7 @@ let soma = numero1 + numero2
 
 console.log("Exercício 02: A soma é:", soma)
 
+// --------------------------------------------------------
 
 let nota1 = 8.8;
 let nota2 = 4.8;
@@ -20,7 +22,7 @@ let nota3 = 3.5;
 let media = (nota1 + nota2 + nota3) / 3;
 console.log("Exercício 03: A média é:", media)
 
-
+// --------------------------------------------------------
 
 let valor1 = 10;
 let valor2 = 5;
@@ -29,10 +31,39 @@ let resultado = (valor1 + valor2) * valor3 / 2;
 
 console.log("Exercício 04: O resultado é:", resultado)
 
+// --------------------------------------------------------
 
 let novaIdade = 25;
 
 
 console.log( "Exercício 05: É maior de idade: ", novaIdade >= 18);
 
+// --------------------------------------------------------
 
+let x = 5;
+let y = 7;
+let auxiliar;
+console.log("exercicio 6:")
+console.log(x, y);
+
+
+auxiliar = x;
+x = y;
+y = auxiliar;
+console.log(x, y);
+
+// --------------------------------------------------------
+
+let dividendo = 50;
+let divisor = 0;
+let divisão = dividendo / divisor;
+console.log("Exercício 05:" , divisão)
+// console.log("abc" / 2); // (exemplo de NaN)
+
+// --------------------------------------------------------
+let contador = 10;
+contador++;
+console.log("exercicio 7:")
+console.log(contador);
+contador--;
+console.log(contador);
